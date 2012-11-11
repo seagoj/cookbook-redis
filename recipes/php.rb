178c11a5b2f20d1fdee3385c17f9d186ddb7ea1c
Jeremy Seago <seagoj@gmail.com>
2012-11-11 17:14:21 -0600

    Adjusting predis::php to create lib file if it doesn't exist

--- recipes/php.rb
+++ recipes/php.rb
@@ -1,8 +1,15 @@
 package 'redis-server'
 
-git "/vagrant/lib/predis" do
-  repository "git://github.com:nrk/predis.git"
-  reference "master"
-  action :checkout
+if !File.exists('/vagrant/lib')
+  execute "mkdir-lib" do
+    command "mkdir /vagrant/lib"
+  end
 end
 
+if !File.exists('/vagrant/predis')
+  git "/vagrant/predis" do
+    repository "git://github.com/nrk/predis.git"
+    reference "master"
+    action :checkout
+  end
+end
