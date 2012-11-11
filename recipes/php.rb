b2d86bd74b590cf919138935e1f655f373b68e28
Jeremy Seago <seagoj@gmail.com>
2012-11-11 17:40:27 -0600

    Changed git path to lib directory

--- recipes/php.rb
+++ recipes/php.rb
@@ -7,7 +7,7 @@ package 'git'
 #end
 
 # if !File.exists('/vagrant/predis')
-  git "/vagrant/predis" do
+  git "/vagrant/lib/predis" do
     repository "git://github.com/nrk/predis.git"
     reference "master"
     action :checkout
