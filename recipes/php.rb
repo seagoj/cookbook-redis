eb306b9a67848901ca356d9dcc3874aaba21bbdc
Jeremy Seago <seagoj@gmail.com>
2012-11-11 17:31:01 -0600

    Removed folder checks

--- recipes/php.rb
+++ recipes/php.rb
@@ -1,15 +1,15 @@
 package 'redis-server'
 
-if !File.exists('/vagrant/lib')
-  execute "mkdir-lib" do
-    command "mkdir /vagrant/lib"
-  end
-end
+#if !File.exists('/vagrant/lib')
+#  execute "mkdir-lib" do
+#    command "mkdir /vagrant/lib"
+#  end
+#end
 
-if !File.exists('/vagrant/predis')
+# if !File.exists('/vagrant/predis')
   git "/vagrant/predis" do
     repository "git://github.com/nrk/predis.git"
     reference "master"
     action :checkout
   end
-end
+# end
