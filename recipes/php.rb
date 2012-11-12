345f85c185292830b6094a34f0983b3bc4184273
Jeremy Seago <seagoj@gmail.com>
2012-11-11 22:44:35 -0600

    move copy Predis/lib dir to /lib/predis

--- recipes/php.rb
+++ recipes/php.rb
@@ -6,9 +6,23 @@ package 'git'
     command "mkdir /vagrant/lib"
   end
 
-  git "/vagrant/lib/predis" do
+  git "/vagrant/lib/temp" do
     not_if {File.exists?('/vagrant/lib/predis')}
     repository "git://github.com/nrk/predis.git"
     reference "master"
     action :checkout
+  end
+  
+  execute "mkdir-lib" do
+    not_if {File.exists?('/vagrant/lib/predis')}
+    command "mkdir /vagrant/lib/predis"
+  end
+  
+  execute "copy-to-lib" do
+    not_if {File.exists?('/vagrant/lib/predis')}
+    command "cp -r /vagrant/lib/temp/lib /vagrant/lib/predis"
+  end
+  
+  execute "rm-temp" do
+    command "rm -r /vagrant/lib/temp"
   end
