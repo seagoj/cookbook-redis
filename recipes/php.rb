8b56e764057376e8bbda2101cc7c3f5ab1da3bf3
Jeremy Seago <seagoj@gmail.com>
2013-09-04 15:42:45 -0500

    Update php.rb

--- recipes/php.rb
+++ recipes/php.rb
@@ -1,29 +1,46 @@
 package 'redis-server'
 package 'git'
 
-  execute "mkdir-lib" do
-    not_if {File.exists?('/vagrant/lib')}
-    command "mkdir /vagrant/lib"
-  end
+directory "/vagrant/lib" do
+  not_if {File.exists?("/vagrant/lib")}
+  owner   node[:redis][:user]
+  group   node[:redis][:group]
+end
 
-  git "/vagrant/lib/temp" do
-    not_if {File.exists?('/vagrant/lib/predis')}
-    repository "git://github.com/nrk/predis.git"
-    reference "master"
-    action :checkout
-  end
+#execute "mkdir-lib" do
+#  not_if {File.exists?('/vagrant/lib')}
+#  command "mkdir /vagrant/lib"
+#end
+
+git "/vagrant/lib/temp" do
+  not_if {File.exists?('/vagrant/lib/predis')}
+  repository "git://github.com/nrk/predis.git"
+  reference "master"
+  action :checkout
+end
+
+directory "/vagrant/lib/predis" do
+  not_if {File.exists?("/vagrant/lib/predis")}
+  owner node[:redis][:user]
+  group node[:redis][:group]
+end
   
-  execute "mkdir-lib" do
-    not_if {File.exists?('/vagrant/lib/predis')}
-    command "mkdir /vagrant/lib/predis"
-  end
+#execute "mkdir-lib" do
+#  not_if {File.exists?('/vagrant/lib/predis')}
+#  command "mkdir /vagrant/lib/predis"
+#end
   
-  execute "copy-to-lib" do
-    not_if {File.exists?('/vagrant/lib/predis')}
-    command "cp -r /vagrant/lib/temp/lib/Predis /vagrant/lib"
-  end
-  
-  execute "rm-temp" do
-    not_if {!File.exists?('/vagrant/lib/temp')}
-    command "rm -r /vagrant/lib/temp"
-  end
+execute "copy-to-lib" do
+  not_if {File.exists?('/vagrant/lib/predis')}
+  command "cp -r /vagrant/lib/temp/lib/Predis /vagrant/lib"
+end
+
+directory "/vagrant/lib/temp" do
+  not_if {!File.exists?("/vagrant/lib/temp")}
+  action :remove
+end
+
+#execute "rm-temp" do
+#  not_if {!File.exists?('/vagrant/lib/temp')}
+#  command "rm -r /vagrant/lib/temp"
+#end
